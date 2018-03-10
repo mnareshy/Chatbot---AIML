f929ade5cad7d836defce8cc4803d4f078868361
Naresh M <mnaresh.y@gmai.com>
2018-03-10 16:14:37 +0530

    BABABot Update

---
+++ src/main/java/com/ai/bot/MyBot.java
@@ -0,0 +1,68 @@
+package com.ai.bot;
+
+import java.io.File;
+
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.History;
+import org.alicebot.ab.MagicBooleans;
+import org.alicebot.ab.MagicStrings;
+import org.alicebot.ab.utils.IOUtils;
+
+public class MyBot {
+	
+	public static void main(String[] args) {
+		
+		
+		File f = new File("");
+		System.out.println(f.getAbsolutePath());
+		
+		Bot bot = new Bot("BABABot", f.getAbsolutePath()+"/src/main/"+ "resources");
+//		bot.setAllPaths("./recources", "bots");
+		
+
+		
+		Chat chatSession =  new Chat(bot, "BABABot");
+		
+		
+		bot.brain.nodeStats();
+		MagicBooleans.trace_mode = false;
+		 
+		
+		
+		String input =  null;
+		String botResponse = null; 
+		
+		boolean botAwake = true;
+		
+		while(botAwake){
+			
+			System.out.println("BABABOT : "+"How Can I Help You :");
+			input = IOUtils.readInputTextLine();
+			
+			if((input == null) && (input.length() < 1))
+				input = MagicStrings.null_input;
+			
+			if((input.equalsIgnoreCase("exit")) || (input.equalsIgnoreCase("quit")))
+				System.exit(0);
+			
+			
+			
+//			chatSession.chat();
+			
+			botResponse = chatSession.multisentenceRespond(input);
+//			History<String>  history = chatSession.inputHistory;
+			
+//			history.printHistory();
+//			System.out.println(input);
+			
+			System.out.println("BABABOT : " + botResponse);
+			
+			
+		}
+				
+				
+	}
+	
+
+}
---
+++ src/main/java/com/ai/bot/Test.java
@@ -0,0 +1,10 @@
+package com.ai.bot;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("SAIRAM");
+	}
+
+}
